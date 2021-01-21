c0f6011d7289580532b5e81900d0fe06927d5ea0
qujdev <75565387+qujdev@users.noreply.github.com>
2021-01-21 21:06:37 +0530

    Update config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -1,26 +1,26 @@
 from tobrot.sample_config import Config
  
 class Config(Config):
-  TG_BOT_TOKEN = "1553212518:AAFMEEdATxOXyp6aUOuajbnIvrxLgfblzSc"
+  TG_BOT_TOKEN = "1562303899:AAEpcbewTCP9xeKTo0SXKl878TsuocS-pcg"
   APP_ID = 2048409 
   API_HASH = "649e419e5fa739da02b649780d492c1b"
-  AUTH_CHANNEL = [-1001437129982]
-  INDEX_LINK = "https://up.iunix.workers.dev"
-  YTDL_COMMAND = "ytdl@iunixbot"
-  LEECH_COMMAND = "leech@iunixbot"
-  CLEAR_THUMBNAIL = "clearthumbnail@iunixbot"
-  SAVE_THUMBNAIL = "savethumbnail@iunixbot"
+  AUTH_CHANNEL = [-1001287210872]
+  INDEX_LINK = ""
+  YTDL_COMMAND = "ytdl@punixbot"
+  LEECH_COMMAND = "leech@nottouse"
+  CLEAR_THUMBNAIL = "clearthumbnail@punixbot"
+  SAVE_THUMBNAIL = "savethumbnail@punixbot"
   OWNER_ID = 1464067320
   RCLONE_CONFIG = """type = drive
 scope = drive
 token = {"access_token":"ya29.a0AfH6SMDQGUcOLEXjCh4j9vuigQ4xh5OLB3YCrk4wVVft6z2n8lzsfSmDb2dWSRRSBEABOUBwFCCFpufHjXD-ON9XQMDVKuWhQ16WWnEFjLcTEVRDm58vAwFtn0A8QtooBDRHZRPwleR3YS3SpZAYtX1iKDqGiUqzY7sdl61-ecc","token_type":"Bearer","refresh_token":"1//0gp-C7DynXw7ACgYIARAAGBASNwF-L9IraFHEJQdx51pVq7l_kLg12cAW80wjgKFKwgS5dSOAx6wlUs5Z9hGcN0i12UoUgYQ4N6I","expiry":"2021-01-07T15:33:00.73411131Z"}
 team_drive = 0AA6n34qudHhyUk9PVA"""
   DESTINATION_FOLDER = "up"
-  GLEECH_COMMAND = "gleech@iunixbot"
-  TELEGRAM_LEECH_COMMAND_G = "tleech@iunixbot"
-  PYTDL_COMMAND_G = "pytdl@iunixbot"
+  GLEECH_COMMAND = "gleech@nottouse"
+  TELEGRAM_LEECH_COMMAND_G = "tleech@punixbot"
+  PYTDL_COMMAND_G = "pytdl@punixbot"
   MAX_TG_SPLIT_FILE_SIZE = 2097152000
-  CANCEL_COMMAND_G = "cancel@iunixbot"
-  STATUS_COMMAND = "status@iunixbot"
-  GET_SIZE_G = "getsize@iunixbot"
-  RENEWME_COMMAND = "renewme@iunixbot" 
+  CANCEL_COMMAND_G = "cancel@punixbot"
+  STATUS_COMMAND = "status"
+  GET_SIZE_G = "getsize@punixbot"
+  RENEWME_COMMAND = "renewme@punixbot" 
